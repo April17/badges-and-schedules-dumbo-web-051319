2bc5c2df5e67ac83dad46079b453eacb0563c120
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 17:37:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+def batch_badge_creator(array)
+  ary = []
+  for i in 0..array.length
+    ary[i] = badge_maker(array[i])
+  end
+  return array
+end
